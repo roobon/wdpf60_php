dd560613d09322bc71e32a12f8a84cfd7cf9d533
SaifulAlam93 <gmsifulalam93@gmail.com>
2024-06-09 13:16:13 +0600

    09 June 2024

---
+++ 09june2024/php_syntax.php
@@ -0,0 +1,15 @@
+<?php 
+    // full syntax
+    echo "Hello World<br>";
+
+?>
+
+<?
+//  short Open Tag
+echo "Hello World with short tag<br>";
+?>
+
+<?="Hello World with short circuit";
+// shot circuit
+
+?>
